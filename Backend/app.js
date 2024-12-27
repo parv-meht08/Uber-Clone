4701381aa2b2adcdb3bba0e0314aeb2744a27b9d
Parv Mehta <mehtaparv21@gmail.com>
2024-12-27 16:16:08 +0530

    Captain Login, Register and Logout Complete

--- Backend/app.js
+++ Backend/app.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const express = require('express')
 const app = express();
 const userRoutes = require('./routes/user.route.js')
+const captainRoutes = require('./routes/captain.route.js')
 const cookieParser = require('cookie-parser');
 
 app.use(cors());
@@ -16,6 +17,7 @@ app.get('/',(req, res)=>{
 })
 
 app.use('/users', userRoutes)
+app.use('/captains', captainRoutes)
 
 
 module.exports = app;
