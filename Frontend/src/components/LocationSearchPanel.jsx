6eeeba7c15f6fb92e0595044fd12bc580de19ec7
Parv Mehta <mehtaparv21@gmail.com>
2024-12-31 23:36:47 +0530

    Added LocationSearchPanel component and vehicle panel functionality

--- Frontend/src/components/LocationSearchPanel.jsx
+++ Frontend/src/components/LocationSearchPanel.jsx
@@ -1,12 +1,36 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-key */
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React from "react";
 
-const LocationSearchPanel = () => {
+const LocationSearchPanel = (props) => {
+  const locations = [
+    "24B, Near Nilkanth Marbels, Piplag Chokdi, Nadiad, Gujarat 387001",
+    "25B, Near Santram Mandir, Nadiad, Gujarat 387001",
+    "2Y, Near Vaniyawad, Vaniyawad Circle, Nadiad, Gujarat 387001",
+    "4B, Near Kideny Hospital, Kideny Hospital, Nadiad, Gujarat 387001",
+  ];
+
   return (
     <div>
-      Location Search Panel
+      {locations.map(function (elem, idx) {
+        return (
+          <div key={idx}
+          onClick={() => {
+            props.setVehiclePanel(true)
+            props.setPanelOpen(false)
+          }}
+            className="flex active:border-2 p-3 border-gray-100 active:border-black rounded-xl items-center justify-start gap-2 pr-3 my-2 mr-[13px]"
+          >
+            <h2 className="bg-[#eee] h-8 w-12 flex items-center justify-center rounded-full">
+              <i className="ri-map-pin-fill"></i>
+            </h2>
+            <h4 className="font-medium">{elem}</h4>
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default LocationSearchPanel
+export default LocationSearchPanel;
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import "remixicon/fonts/remixicon.css";
@@ -9,8 +9,10 @@ const Home = () => {
   const [pickUp, setPickUp] = useState("");
   const [dest, setdest] = useState("");
   const [panelOpen, setPanelOpen] = useState(false);
+  const vehiclePanelRef = useRef(null);
   const panelRef = useRef(null);
   const panelClose = useRef(null);
+  const [vehiclePanel, setVehiclePanel] = useState(false);
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -37,8 +39,23 @@ const Home = () => {
     [panelOpen]
   );
 
+  useGSAP(
+    function () {
+      if (vehiclePanel) {
+        gsap.to(vehiclePanelRef.current, {
+          transform: 'translateY(0)',
+        });
+      } else {
+        gsap.to(vehiclePanelRef.current, {
+          transform: "translateY(100%)",
+        });
+      }
+    },
+    [vehiclePanel]
+  );
+  
   return (
-    <div className="h-screen relative">
+    <div className="h-screen relative overflow-hidden">
       <img
         className="w-20 absolute left-5 top-5"
         src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"
@@ -93,10 +110,82 @@ const Home = () => {
             />
           </form>
         </div>
-        <div ref={panelRef} className="o h-0 bg-white">
-          <LocationSearchPanel />
+
+        <div ref={panelRef} className="pl-5 h-0 bg-white">
+          <LocationSearchPanel
+           setPanelOpen={setPanelOpen}
+           setVehiclePanel={setVehiclePanel}
+          />
         </div>
       </div>
+
+      <div
+        ref={vehiclePanelRef}
+        className="fixed w-full z-10 translate-y-full bg-white px-3 py-10 pt-14 bottom-0"
+      >
+       <h5 onClick={()=>{
+        setVehiclePanel(false);
+      }} className="p-1 text-center w-[93%] absolute top-0"><i className="text-3xl text-gray-200 ri-arrow-down-s-line"></i></h5>
+        <h3 className="text-2xl font-semibold mb-3">Choose a Vehicle</h3>
+
+        <div className="flex border-2 active:border-black bg-gray-100 rounded-lg w-full items-center justify-between p-3 gap-1 mb-3">
+          <img
+            className="h-8"
+            src="https://static.vecteezy.com/system/resources/thumbnails/046/836/811/small/side-view-white-car-png.png"
+            alt=""
+          />
+          <div className="w-1/2">
+            <h4 className="font-medium text-sm">
+              Uber Go{" "}
+              <span>
+                <i className="ri-user-line"></i>4
+              </span>
+            </h4>
+            <h5 className="text-gray-600 font-medium text-sm">2 mins away</h5>
+            <p className="text-xs text-gray-500">Affordable Compact rides</p>
+          </div>
+          <h2 className="text-lg font-semibold">₹156.3</h2>
+        </div>
+
+        <div className="flex border-2 active:border-black bg-gray-100 rounded-lg w-full items-center justify-between p-3 gap-1 mb-3">
+          <img
+            className="h-10"
+            src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1649231091/assets/2c/7fa194-c954-49b2-9c6d-a3b8601370f5/original/Uber_Moto_Orange_312x208_pixels_Mobile.png"
+            alt=""
+          />
+          <div className="w-1/2">
+            <h4 className="font-medium text-sm">
+              Moto{" "}
+              <span>
+                <i className="ri-user-line"></i>1
+              </span>
+            </h4>
+            <h5 className="text-gray-600 font-medium text-sm">3 mins away</h5>
+            <p className="text-xs text-gray-500">Affordable MotorCycle ride</p>
+          </div>
+          <h2 className="text-lg font-semibold">₹65</h2>
+        </div>
+
+        <div className="flex border-2 active:border-black bg-gray-100 rounded-lg w-full items-center justify-between p-3 gap-1 mb-3">
+          <img
+            className="h-10"
+            src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1648431773/assets/1d/db8c56-0204-4ce4-81ce-56a11a07fe98/original/Uber_Auto_558x372_pixels_Desktop.png"
+            alt=""
+          />
+          <div className="w-1/2">
+            <h4 className="font-medium text-sm">
+              Uber Auto{" "}
+              <span>
+                <i className="ri-user-line"></i>1
+              </span>
+            </h4>
+            <h5 className="text-gray-600 font-medium text-sm">3 mins away</h5>
+            <p className="text-xs text-gray-500">Affordable Auto ride</p>
+          </div>
+          <h2 className="text-lg font-semibold">₹118.6</h2>
+        </div>
+      </div>
+
     </div>
   );
 };
